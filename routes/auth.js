02e5621275a7beee783c40def7598067b0678f4c
sevkioruc <sevkioruc34@gmail.com>
2022-05-05 19:38:39 +0300

    Middleware added for validation of username and password

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ const router = require('express').Router()
 const authContoller = require('../controllers/auth')
 const passport = require('passport');
 
+const { validateUsername } = require('../middlewares/usernameValidator')
+const { validatePassword } = require('../middlewares/passwordValidator')
+
 module.exports = app => {
-	router.post('/register', authContoller.register)
+	router.post('/register', validateUsername, validatePassword, authContoller.register)
 	router.post('/login', passport.authenticate('local', { session: false }), authContoller.login)
 	router.get('/whoami', passport.authenticate('jwt', { session: false }), authContoller.whoami)
 
